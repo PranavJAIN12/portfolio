5eee1da83955de5cb5f0de48bffeccaf0a9e0a66
PranavJAIN12 <94856533+PranavJAIN12@users.noreply.github.com>
2024-02-24 12:07:55 +0530

    navbar done

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
+import React from 'react'
+import './Navbar.css';
+import { Link } from 'react-scroll';
+
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+        <h1 className='navbar-head'>Pranav Jain</h1>
+            <div className='desktopMenu'>
+                <Link className='desktopMenuListItem'>Home</Link>
+                <Link className='desktopMenuListItem'>About</Link>
+                <Link className='desktopMenuListItem'>Portfolio</Link>
+                <Link className='desktopMenuListItem'>Clients</Link>
+            </div>
+            
+                <button className="navbarBtn" role='button'>Contact Me</button>
+            
+    </nav>
+  )
+}
