764433a9455313f4266c12112586489c76383d44
PranavJAIN12 <94856533+PranavJAIN12@users.noreply.github.com>
2024-02-25 23:08:31 +0530

    commit

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -3,6 +3,8 @@ import React from 'react'
 import './Intro.css'
 import photo from '../../assets/man.png'
 // import { Link } from 'react-scroll' 
+import github from '../../assets/github.png'
+import linkedin from '../../assets/linkedin.png'
 
 export default function Intro() {
   return (
@@ -12,6 +14,10 @@ export default function Intro() {
             <span className='name'>I'm <span className='name-name'style={{color:'#DA0037'}}>Pranav</span><br/>A Web Developer</span>
             <p className='intro-para'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae eum dolor itaque ad at, voluptate deleniti, alias necessitatibus magni veritatis recusandae veniam perspiciatis!</p>
         <button className='hire-btn'>Hire me</button>
+        <div className='links'>
+           <a href='www.github.com/PranavJain12'> <img className='link-item' style={{backgroundColor:'white ', borderRadius:'100%'}} src={github}></img></a>
+            <img className='link-item' src={linkedin}></img>
+        </div>
         </div>
         <img className='bg' src={photo} />
     </section>
