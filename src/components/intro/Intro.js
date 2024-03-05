96070b64a507598c776ea7d99f6ffb860d58d18d
PranavJAIN12 <94856533+PranavJAIN12@users.noreply.github.com>
2024-03-06 00:02:15 +0530

    changes made

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import './Intro.css'
@@ -7,13 +8,17 @@ import github from '../../assets/github.png'
 import linkedin from '../../assets/linkedin.png'
 
 export default function Intro() {
+  const contact = () => {
+    console.log("going to contact section");
+    document.querySelector('.contact').scrollIntoView({ behavior: 'smooth' });
+};
   return (
     <section id='intro'>
         <div className="introContent">
             <span className='hello'>Hello,</span><br/>
             <span className='name'>I'm <span className='name-name'style={{color:'#DA0037'}}>Pranav</span><br/>A Web Developer</span>
             <p className='intro-para'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae eum dolor itaque ad at, voluptate deleniti, alias necessitatibus magni veritatis recusandae veniam perspiciatis!</p>
-        <button className='hire-btn'>Hire me</button>
+        <button className='hire-btn' role='button' onClick={contact}>Hire me</button>
         <div className='links'>
            <a href='https://www.github.com/PranavJain12' target='_blank' rel="noopener noreferrer"> <img className='link-item' style={{backgroundColor:'white ', borderRadius:'100%'}} src={github}></img></a>
             <a href='https://www.linkedin.com/in/pranav-jain-32179722a/' ><img className='link-item' src={linkedin}></img></a>
