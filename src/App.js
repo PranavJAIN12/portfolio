f323d442de840318ddadb5433acb9f1ce63c5558
PranavJAIN12 <94856533+PranavJAIN12@users.noreply.github.com>
2024-03-26 23:05:15 +0530

    framer mition

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-
-// import './App.css';
+import React, { useEffect, useRef } from 'react';
 import Navbar from "./components/Navbar";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import Intro from "./components/intro/Intro";
 import 'react-perfect-scrollbar/dist/css/styles.css';
-import PerfectScrollbar from 'react-perfect-scrollbar'
+import PerfectScrollbar from 'react-perfect-scrollbar';
 import Portfolio from "./components/portfolio/Portfolio";
 import Details from "./components/details/Details";
+import { motion, useInView, useAnimation } from 'framer-motion';
 
 function App() {
+  
+
   return (
-    <>
-    <PerfectScrollbar onScrollDown={container => console.log(`scrolled to: ${container.scrollTop}.`)}>
-      <Navbar/>
-      <Intro/>
-      <About/>
-      <Portfolio/>
-      <Details/>
-      <Contact/>
-      <Footer/>
+    <div>
+      <PerfectScrollbar onScrollDown={container => console.log(`scrolled to: ${container.scrollTop}.`)}>
+        <Navbar />
+        <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.6, delay: 0.25 }}>
+          <Intro />
+          <About />
+          <Portfolio />
+          <Details />
+          <Contact />
+          <Footer />
+        </motion.div>
       </PerfectScrollbar>
-    </>
+    </div>
   );
 }
 
--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,32 +1,46 @@
 /* eslint-disable jsx-a11y/no-redundant-roles */
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react'
-import './Intro.css'
-import photo from '../../assets/1.png'
-// import { Link } from 'react-scroll' 
-import github from '../../assets/github.png'
-import linkedin from '../../assets/linkedin.png'
+import React from 'react';
+import './Intro.css';
+import photo from '../../assets/1.png';
+import github from '../../assets/github.png';
+import linkedin from '../../assets/linkedin.png';
+import { motion } from 'framer-motion';
 
 export default function Intro() {
   const contact = () => {
     console.log("going to contact section");
     document.querySelector('.contact').scrollIntoView({ behavior: 'smooth' });
-};
+  };
+
   return (
     <section id='intro'>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.6, delay: 0.25 }}>
         <div className="introContent">
-            <span className='hello'>Hello,</span><br/>
-            <span className='name'>I'm <span className='name-name'style={{color:'#DA0037'}}>Pranav</span><br/>A Web Developer</span>
-            <p className='intro-para'>Welcome to Pranav Jain's Portfolio<br></br>
-Discover my projects, explore my skills, and get to know me better.<br></br>
-A passionate developer with a knack for creating innovative solutions.</p>
-        <button className='hire-btn' role='button' onClick={contact}>Hire me</button>
-        <div className='links'>
-           <a href='https://www.github.com/PranavJain12' target='_blank' rel="noopener noreferrer"> <img className='link-item' style={{backgroundColor:'white ', borderRadius:'100%'}} src={github}></img></a>
-            <a href='https://www.linkedin.com/in/pranav-jain-32179722a/' ><img className='link-item' src={linkedin}></img></a>
+          <span className='hello'>Hello,</span><br />
+          <span className='name'>I'm <span className='name-name' style={{ color: '#DA0037' }}>Pranav</span><br />A Web Developer</span>
+          <p className='intro-para'>Welcome to Pranav Jain's Portfolio<br />
+            Discover my projects, explore my skills, and get to know me better.<br />
+            A passionate developer with a knack for creating innovative solutions.</p>
+          <button className='hire-btn' role='button' onClick={contact}>Hire me</button>
+          <div className='links'>
+            <a href='https://www.github.com/PranavJain12' target='_blank' rel="noopener noreferrer">
+              <img className='link-item' style={{ backgroundColor: 'white', borderRadius: '100%' }} src={github} alt="GitHub" />
+            </a>
+            <a href='https://www.linkedin.com/in/pranav-jain-32179722a/' target='_blank' rel="noopener noreferrer">
+              <img className='link-item' src={linkedin} alt="LinkedIn" />
+            </a>
+          </div>
         </div>
-        </div>
-        <img className='bg' src={photo} />
+      </motion.div>
+      <img className='bg' src={photo} alt="Background" />
     </section>
-  )
+  );
 }
